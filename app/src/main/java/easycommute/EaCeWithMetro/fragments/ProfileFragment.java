879f2e9d16b5f897118f381e6a4d5cbb0b5e259f
Aditi Goswami <aditi.goswami@gmail.com>
2020-01-20 06:50:20 +0530

    Added Code Review Comments for Naveen. Open items to discuss between Naveen & Aditi

--- app/src/main/java/easycommute/EaCeWithMetro/fragments/ProfileFragment.java
+++ app/src/main/java/easycommute/EaCeWithMetro/fragments/ProfileFragment.java
@@ -31,6 +31,16 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
+// All the external communication string (which would be visible to
+// the android users, be it in exceptions or screen title or anything else,
+// They need to be standardized and kept in one file. This would prepare
+// our app to be multilingual.
+// Aditi will mark those as POTENTIAL_MULTILINGUAL_STRINGS in her comments
+
+// At some places we are prepared for us being multilingual in this fragment
+// Example: verifyCompany function
+// throw new RuntimeException(getString(R.string.warning_company_empty));
+
 public class ProfileFragment extends BaseFragment {
     private RadioGroup radioGroup;
     private EditText phoneEdt;
@@ -40,7 +50,13 @@ public class ProfileFragment extends BaseFragment {
     TextView txtBookingId;
     RelativeLayout rlLayout;
     String spinnerText;
-    int cityId=0;
+
+    // TODO_NAVEEN :- Add the comment to mention why 0 is being used and not some other value
+    // Shall we be putting all the hardcoded values for this class in a seperate file? if yes,
+    // decide what that file would be & do the rest of implementation.
+
+    int cityId = 0;
+
     List<Integer> cityIdList = new ArrayList<>();
     PreferenceManager preferenceManager;
 
@@ -60,12 +76,13 @@ public class ProfileFragment extends BaseFragment {
 
         phoneEdt = (EditText) getView().findViewById(R.id.inputMobile);
         emailEdt = (EditText) getView().findViewById(R.id.inputEmail);
-        preferenceManager=new PreferenceManager(getActivity());
         radioGroup = (RadioGroup) getView().findViewById(R.id.myRadioGroup);
         nameEdt  = (EditText) getView().findViewById(R.id.inputName);
-        spinnerCityList=(Spinner) getView().findViewById(R.id.spinnerCityList);
-        txtBookingId=(TextView)getView().findViewById(R.id.txtBookingId);
-        rlLayout=(RelativeLayout)getView().findViewById(R.id.rlLayout);
+        spinnerCityList = (Spinner) getView().findViewById(R.id.spinnerCityList);
+        txtBookingId = (TextView)getView().findViewById(R.id.txtBookingId);
+        rlLayout = (RelativeLayout)getView().findViewById(R.id.rlLayout);
+
+        preferenceManager = new PreferenceManager(getActivity());
 
         /*rlLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,9 +107,42 @@ public class ProfileFragment extends BaseFragment {
         txtBookingId.setBackgroundColor(getResources().getColor(R.color.ticket_top));
         txtBookingId.setText(preferenceManager.getBookingMessage());
 */
+        getView().findViewById(R.id.btn_edit_profile).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                String gender = radioGroup.getCheckedRadioButtonId()
+                        == R.id.male ? "M" : "F";
+
+                String email = emailEdt.getText().toString();
+                String name  = nameEdt.getText().toString();
+                Commuter commuter = EasySingleton.getInstance()
+                        .getCommuter();
+                try {
+                    verifyEmail(email);
+                    verifyInput(name);
+
+                    commuter.gender = gender;
+                    commuter.email = email;
+                    commuter.name = name;
+                    commuter.cityId = cityId;
+
+                    if(cityId!=0) {
+                        updateCommuter(commuter);
+                    }
+                    else {
+                        // POTENTIAL_MULTILINGUAL_STRINGS
+                        showToast("Please select city");
+                    }
+                } catch (Exception e) {
+                    showToast(e.getMessage());
+                }
+            }
+        });
 
         getCityList();
+
+
         spinnerCityList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
@@ -113,39 +163,12 @@ public class ProfileFragment extends BaseFragment {
             }
         });
 
-        getView().findViewById(R.id.btn_edit_profile).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String gender = radioGroup.getCheckedRadioButtonId()
-                        == R.id.male ? "M" : "F";
-
-                String email = emailEdt.getText().toString();
-                String name  = nameEdt.getText().toString();
-                Commuter commuter = EasySingleton.getInstance()
-                        .getCommuter();
-                try {
-                    verifyEmail(email);
-                    verifyInput(name);
-
-                    commuter.gender = gender;
-                    commuter.email = email;
-                    commuter.name = name;
-                    commuter.cityId=cityId;
-
-                    if(cityId!=0) {
-                        updateCommuter(commuter);
-                    }
-                    else {
-                        showToast("Please select city");
-                    }
-                } catch (Exception e) {
-                    showToast(e.getMessage());
-                }
-            }
-        });
         updateProfile();
     }
+
+    //TODO_NAVEEN: Please add comment here to explain what this private
+    // function does. And does it have some prerequsites? Like this updateProfile
+    // function assumes that the RadioButton of this view is initialized.
 
     private void updateProfile() {
         Commuter commuter = EasySingleton.getInstance()
@@ -153,10 +176,20 @@ public class ProfileFragment extends BaseFragment {
         emailEdt.setText(commuter.email);
         phoneEdt.setText(commuter.phone);
         nameEdt.setText(commuter.name);
+
+        //TODO_NAVEEN: There is a better way to write this, figure it out & discuss.
         ((RadioButton) getView().findViewById(commuter.gender.equals("M") ? R.id.male
                 : R.id.female)).setChecked(true);
-        cityId=commuter.cityId;
+
+        // So, here we are setting the cityId variable as per the information from
+        // oommuters -- Then is this not fetchProfile? Why is it called update?
+        // As I see it, we are setting local variable of our class by looking up the
+        // backend. Naveen: What do you think?
+        cityId = commuter.cityId;
     }
+
+    //TODO_NAVEEN: Please add comment here to explain what this private
+    // function does. And does it have some prerequsites?
 
     private void updateCommuter(final Commuter commuter) {
         commuter.commuterId = getCommuterId();
@@ -176,6 +209,10 @@ public class ProfileFragment extends BaseFragment {
                     }
                 }, errorHandler);
     }
+
+    //TODO_NAVEEN: Please add comment here to explain what the output of this function would be?
+    // Give sample output for clarity purpose in the comment.
+
     private void getCityList(){
         showProgressBar();
 
@@ -188,11 +225,16 @@ public class ProfileFragment extends BaseFragment {
                         Commuter commuter=preferenceManager.getCommuter();
                         cityId=commuter.cityId;
                         String cityNameList[]=new String[cityList.cityDataList.size()+1];
+
+                        //Hardcoded values in code! - Move to the constant files please.
                         cityNameList[0]="Select City";
                         cityIdList.add(0);
-                        String dummy="";
+                        String dummy=""; // dont call a variable dummy please :)
 
-                        for(int i=0;i<cityList.cityDataList.size();i++){
+                        //NOTICE: How aditi has added spaces to code to look cleaner and readable.
+                        //TODO_NAVEEN: Follow my example to fix readability of code in all our products.
+                        for(int i = 0; i < cityList.cityDataList.size(); i++)   {
+
                             cityNameList[i+1]=cityList.cityDataList.get(i).name;
                             cityIdList.add(cityList.cityDataList.get(i).id);
                             if(cityList.cityDataList.get(i).id==cityId){
@@ -217,18 +259,23 @@ public class ProfileFragment extends BaseFragment {
 
     private void verifyEmail(String email) {
         if (email.isEmpty()) {
+            // POTENTIAL_MULTILINGUAL_STRINGS
             throw new RuntimeException(getString(R.string.warning_email_empty));
         } else if (!Pattern.compile(AppConstants.EMAIL_PATTERN).matcher(email).matches()) {
+            // POTENTIAL_MULTILINGUAL_STRINGS
             throw new RuntimeException(getString(R.string.warning_email_not_proper));
         }
     }
 
     private void verifyInput(String input) {
         if (input.isEmpty()) {
+            //POTENTIAL_MULTILINGUAL_STRINGS
             throw new RuntimeException(getString(R.string.warning_name_empty));
         } else if (Pattern.compile("[^A-Za-z0-9 ]").matcher(input).find()) {
+            //POTENTIAL_MULTILINGUAL_STRINGS
             throw new RuntimeException(getString(R.string.warning_name_spl_char));
         } else if (Character.isDigit(input.charAt(0))) {
+            //POTENTIAL_MULTILINGUAL_STRINGS
             throw new RuntimeException(getString(R.string.warning_name_digit));
         }
     }
@@ -242,6 +289,7 @@ public class ProfileFragment extends BaseFragment {
 
     @Override
     protected int getTitle() {
+        // POTENTIAL_MULTILINGUAL_STRINGS
         return R.string.title_profile;
     }
 }
