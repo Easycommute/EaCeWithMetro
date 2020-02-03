f149af68d3d159a27c64db340a8bde2d6eb2f1ba
Aditi Goswami <aditi.goswami@gmail.com>
2020-02-03 05:46:36 +0530

    Cleanup of UI of My History Screen

--- app/src/main/java/easycommute/EaCeWithMetro/adapter/HistoryAdapter.java
+++ app/src/main/java/easycommute/EaCeWithMetro/adapter/HistoryAdapter.java
@@ -3,6 +3,7 @@ package easycommute.EaCeWithMetro.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,6 +12,7 @@ import java.util.List;
 
 import easycommute.EaCeWithMetro.R;
 import easycommute.EaCeWithMetro.models.Myhistory.Response;
+import easycommute.EaCeWithMetro.models.Myhistory.TxnEntry;
 
 /**
  * Created by Ram Prasad on 11/15/2015.
@@ -37,13 +39,26 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHold
         Response response = historyResponse.get(position);
        if (response.getTxnEntry().getTxnValue()<0)   // Debit
        {
-             holder.txtCredit.setText(response.getTxnEntry().getTransactionDescription().getDescription()+" "+String.valueOf(response.getTxnEntry().getTxnValue())+" | "+ " "+response.getTimeEntry());
+           TxnEntry T =  response.getTxnEntry();
+           Integer value = -1 * T.getTxnValue();
+
+           holder.txtDebit.setText("Rs. " + String.valueOf(value));
+           holder.txtDebitDesc.setText(T.getTransactionDescription().getDescription());
+           holder.txtDebitTime.setText(response.getTimeEntry());
+           holder.CreditView.setVisibility(View.INVISIBLE);
        }
        else   // credit
        {
-           holder.txtDebit.setText(response.getTimeEntry()  +" | "+ String.valueOf(response.getTxnEntry().getTxnValue())+ " "+response.getTxnEntry().getTransactionDescription().getDescription());
+           TxnEntry T =  response.getTxnEntry();
+           Integer value = T.getTxnValue();
+
+           holder.txtCredit.setText("Rs. " + String.valueOf(value));
+           holder.txtCreditDesc.setText(T.getTransactionDescription().getDescription());
+           holder.txtCreditTime.setText(response.getTimeEntry());
+           holder.DebitView.setVisibility(View.INVISIBLE);
+
        }
-       holder.itemView.findViewById(R.id.offset).setVisibility(position != 0 ? View.VISIBLE : View.INVISIBLE);
+
     }
 
     @Override
@@ -63,12 +78,22 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHold
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
-        protected TextView txtCredit;
-        protected TextView txtDebit;
+        protected TextView txtCredit, txtCreditTime, txtCreditDesc;
+        protected TextView txtDebit, txtDebitTime, txtDebitDesc;
+        protected ImageView CreditView, DebitView;
+
+
         public ViewHolder(View itemView, int viewType) {
             super(itemView);
             txtCredit = (TextView) itemView.findViewById(R.id.txtCredit);
             txtDebit = (TextView) itemView.findViewById(R.id.txtDebit);
+            txtCreditDesc = (TextView) itemView.findViewById(R.id.txtCreditDesc);
+            txtDebitDesc = (TextView) itemView.findViewById(R.id.txtDebitDesc);
+            txtCreditTime = (TextView) itemView.findViewById(R.id.txtCreditTime);
+            txtDebitTime = (TextView) itemView.findViewById(R.id.txtDebitTime);
+
+            CreditView = (ImageView) itemView.findViewById(R.id.CreditImg);
+            DebitView = (ImageView) itemView.findViewById(R.id.DebitImg);
         }
     }
 }
